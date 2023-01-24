15f542eec41103811399fc124e2ae283fc4318f9
Julio Reyes <juliojesusreyes@gmail.com>
2023-01-24 18:23:45 -0300

    Terminando juego consola, falta implementar score

--- main.js
+++ main.js
@@ -9,55 +9,55 @@ function JuegaCPU () {
 
 function EmpezarRonda (seleccionJugador, seleccionCPU) {
 
-    if (seleccionJugador === "piedra" && seleccionCPU === "Papel") {
+    if (seleccionJugador == "piedra" && seleccionCPU == "Papel") {
 
         return "Perdiste! El papel le gana a la piedra!";
         
     } 
     
-    else if (seleccionJugador === "piedra" && seleccionCPU === "Piedra") {
+    else if (seleccionJugador == "piedra" && seleccionCPU == "Piedra") {
 
         return "Empate! Piedras iguales.";
 
     }
 
-    else if (seleccionJugador === "piedra" && seleccionCPU === "Tijeras") {
+    else if (seleccionJugador == "piedra" && seleccionCPU == "Tijeras") {
 
         return "Ganaste, Piedra le gana a Tijeras!";
         
     }
 
-    else if (seleccionJugador === "papel" && seleccionCPU === "Papel") {
+    else if (seleccionJugador == "papel" && seleccionCPU == "Papel") {
 
         return "Empate! Papeles iguales.";
 
     }
 
-    else if (seleccionJugador === "papel" && seleccionCPU === "Piedra") {
+    else if (seleccionJugador == "papel" && seleccionCPU == "Piedra") {
 
         return "Ganaste, Papel le gana a Piedra!";
 
     }
 
-    else if (seleccionJugador === "papel" && seleccionCPU === "Tijeras") {
+    else if (seleccionJugador == "papel" && seleccionCPU == "Tijeras") {
 
         return "Perdiste, El papel es cortado por las Tijeras!";
 
     }
 
-    else if (seleccionJugador === "tijeras" && seleccionCPU === "Pïedra") {
+    else if (seleccionJugador == "tijeras" && seleccionCPU == "Pïedra") {
 
         return "Perdiste, Tijeras es aplastada por la Piedra.";
 
     }
 
-    else if (seleccionJugador === "tijeras" && seleccionCPU === "Papel") {
+    else if (seleccionJugador == "tijeras" && seleccionCPU == "Papel") {
 
         return "Ganaste! Tijeras corta el Papel.";
 
     }
 
-    else if (seleccionJugador === "tijeras" && seleccionCPU === "Tijeras") {
+    else if (seleccionJugador == "tijeras" && seleccionCPU == "Tijeras") {
 
         return "Empate! Tijeras iguales.";
 
@@ -65,27 +65,22 @@ function EmpezarRonda (seleccionJugador, seleccionCPU) {
     
     else {
 
-        return "Seleccion Invalida";
+        return "Selección Invalida.";
 
     }
     
 }
 
+function Juego () {
 
+    for (let ronda = 0; ronda < 5; ronda++) {
 
-
-const seleccionJugador = prompt ("Seleccione su jugada entre piedra, papel ó tijeras?");
-const seleccionCPU = getComputerChoice ();
-console.log (playRound(seleccionJugador, seleccionCPU));
-
-function game () {
-
-    for (let i = 0; i < 5; i++) {
-
-        playRound(seleccionJugador, seleccionCPU)
-
+        const seleccionJugador = prompt ("Seleccione su jugada entre piedra, papel ó tijeras?").toLowerCase();
+        const seleccionCPU = JuegaCPU ();
+        console.log (EmpezarRonda (seleccionJugador, seleccionCPU));
+        
     }
 
 }
 
-game ();
+Juego ();
